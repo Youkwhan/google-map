f072e13ad183ff70d99e2ad583324e0b38a50e84
Youkwhan <37788922+Youkwhan@users.noreply.github.com>
2022-07-19 23:23:23 -0400

    Loading our Google Map script

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-
-function App() {
-	//This is a change for github test
-	return <div> Map</div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "./components/Map";
+
+function App() {
+	//LoadScript - Loads the Google Maps JavaScript API script
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+		libraries: ["places"],
+	});
+
+	if (!isLoaded) return <div>Loading...</div>;
+	return <div> Map</div>;
+}
+
+export default App;
